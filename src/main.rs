3570a115cf77e0d955830d994f959428c2068056
Norman Sutorius <us.idev.student@googlemail.com>
2019-05-05 10:52:07 +0200

    Add some print examples

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,21 @@ fn main() {
 }
 
 fn test() {
+    let name = "Bricktop";
     println!("Hello from test");
+    // print formatter
+    print!("{} mal hello \n", 27);
+    //print some specific values
+    println!("My name is {firstparam}!", firstparam = name);
+
+    // disable the linter
+    #[allow(dead_code)]
+    struct Structure(i32);
+    // add the debug formatter for print
+    #[derive(Debug)]
+
+    // However, custom types such as this structure require more complicated
+    // handling. This will not work.
+    println!("This struct `{:?}` won't print...", Structure(3));
+    // FIXME ^ Comment out this line.
 }
